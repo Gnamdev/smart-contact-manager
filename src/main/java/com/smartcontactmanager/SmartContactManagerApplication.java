2679c321702495b68ff2d9c46704da3953a03eb5
Gnamdev <goutamnamdev8120@gmail.com>
2024-08-19 16:49:43 +0530

    update properties file

--- src/main/java/com/smartcontactmanager/SmartContactManagerApplication.java
+++ src/main/java/com/smartcontactmanager/SmartContactManagerApplication.java
@@ -3,10 +3,16 @@ package com.smartcontactmanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class SmartContactManagerApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+		System.out.println("DB_URL: " + System.getenv("DB_URL"));
+
 		SpringApplication.run(SmartContactManagerApplication.class, args);
 	}
 
