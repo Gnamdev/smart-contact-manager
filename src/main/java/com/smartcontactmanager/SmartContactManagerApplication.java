ac6211c5a800fd0f121f654e26508dd2ccfa8a37
Gnamdev <goutamnamdev8120@gmail.com>
2024-09-10 22:10:15 +0530

    Normalize line endings

--- src/main/java/com/smartcontactmanager/SmartContactManagerApplication.java
+++ src/main/java/com/smartcontactmanager/SmartContactManagerApplication.java
@@ -3,6 +3,8 @@ package com.smartcontactmanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class SmartContactManagerApplication {
 
